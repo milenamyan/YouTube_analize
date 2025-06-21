18aacc99cf254bc21e6dcfe9ee8cfd08f23bc4da
milenamyan <milena.mehrabyan2005@gmail.com>
2025-06-21 05:01:32 +0400

    Returning dict with informations

--- analizeVideo.py
+++ analizeVideo.py
@@ -1,7 +1,26 @@
 from pytubefix import YouTube
 
-URL = "https://www.youtube.com/watch?v=tvZzjcaGs1U&list=PLfLD2TpGxVUwNhdJWh3lpAJLFBjV6ilqv&index=17"
-yt = YouTube(URL)
+URL = "https://www.youtube.com/watch?v=sNcLm2MtvwE&list=PLQLz3vJxwofh8KSaJ7FoA5Bw2Zdvyo4S_&index=6"
 
-print(yt.title)
-print('Hello')
+
+def get_info(url: str) -> dict:
+    """Fetches basic info about a YouTube video.
+
+    Note:
+        Returns 1.title, 2.views, etc...
+
+    Args:
+        url (str): The URL of the YouTube video.
+
+    Returns:
+        dict: A dictionary containing the video's title, view, etc...
+    """
+    yt = YouTube(url)
+    data = {
+        "title": yt.title,
+        "views": yt.views,
+        "length_seconds": yt.length
+    }
+    return data
+
+print(get_info(URL))
