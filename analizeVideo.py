47c00f58a3e12fe72bfd88d6e613775fb5236f94
milenamyan <milena.mehrabyan2005@gmail.com>
2025-06-21 04:38:16 +0400

    for testing terminal in VS

--- analizeVideo.py
+++ analizeVideo.py
@@ -2,4 +2,6 @@ from pytubefix import YouTube
 
 URL = "https://www.youtube.com/watch?v=tvZzjcaGs1U&list=PLfLD2TpGxVUwNhdJWh3lpAJLFBjV6ilqv&index=17"
 yt = YouTube(URL)
+
 print(yt.title)
+print('Hello')
